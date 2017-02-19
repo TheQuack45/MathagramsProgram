0ef51d8111f8f41f593a85b4fd6d16c0edecb26e
Daniel Quackenbush <TheQuack45@gmail.com>
2017-02-19 18:17:12 -0500

    Pulled out some aspects of the Shuffle method into separate methods to appease the mighty CollegeBoard.

--- MathagramsProgram/Program.cs
+++ MathagramsProgram/Program.cs
@@ -9,7 +9,6 @@ namespace MathagramsProgram
 {
     class Program
     {
-        private enum MATHAGRAMS_TYPE { Single, Double, Triple };
         private const string DIGITS_MATCH_PATTERN = @"[\dx]{3}";
         private const string EXPRESSION_FORMAT = @"{0} + {1} = {2}";
         private const string DOUBLE_EXPRESSION_FORMAT = @"{0} + {1} + {2} + {3} = {4} + {5}";
@@ -23,45 +22,6 @@ namespace MathagramsProgram
             Console.WriteLine("Solution: " + solved);
 
             Console.ReadKey();
-        }
-
-        public static string SolveDoubleMathagram(string expression)
-        {
-            List<string> nums = new List<string>((string[])DOUBLE_NUMBERS.Clone());
-            char[] exprArr = expression.ToCharArray();
-
-            foreach (char c in exprArr)
-            {
-                nums.Remove(c.ToString());
-            }
-            // nums now contains the numbers we can use to form the equation.
-
-            while (true)
-            {
-                nums = Shuffle(nums);
-
-                string[] digits = Regex.Matches(expression, DIGITS_MATCH_PATTERN).OfType<Match>().Select(m => m.Groups[0].Value).ToArray<string>();
-
-                int numsIndex = 0;
-                for (int i = 0; i < digits.Length; i++)
-                {
-                    char[] digitsArr = digits[i].ToCharArray();
-                    for (int j = 0; j < digits[i].Length; j++)
-                    {
-                        if (digitsArr[j] == 'x')
-                        {
-                            digitsArr[j] = nums[numsIndex][0];
-                            numsIndex++;
-                        }
-                    }
-                    digits[i] = new string(digitsArr);
-                }
-
-                if (Int32.Parse(digits[0]) + Int32.Parse(digits[1]) + Int32.Parse(digits[2]) + Int32.Parse(digits[3]) == Int32.Parse(digits[4]) + Int32.Parse(digits[5]))
-                {
-                    return String.Format(DOUBLE_EXPRESSION_FORMAT, digits[0], digits[1], digits[2], digits[3], digits[4], digits[5]);
-                }
-            }
         }
 
         public static string SolveMathagram(string expression)
@@ -113,13 +73,31 @@ namespace MathagramsProgram
             {
                 do
                 {
-                    int rdmIndex = rdm.Next(stringArr.Length);
-                    if (!outputStringArr.Contains<string>(stringArr[rdmIndex]))
-                        { outputStringArr[i] = stringArr[rdmIndex]; }
+                    int rdmIndex = GenRandom(rdm, stringArr.Length);
+                    if (!ArrayContains(outputStringArr, stringArr[rdmIndex]))
+                    { outputStringArr[i] = stringArr[rdmIndex]; }
                 } while (outputStringArr[i] == null);
             }
 
             return outputStringArr.ToList<string>();
+        }
+
+        private static int GenRandom(Random gen, int max)
+        {
+            return gen.Next(max);
+        }
+
+        private static bool ArrayContains(string[] arr, string value)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i].Equals(value))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
